8bc528e1d57b97c393e95d6788e7f287d1b9d4a4
Ryan Matthews <ryan.matthews.professional@gmail.com>
2020-04-24 15:23:37 -0400

    update styles

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import image from './img/img1.jpg'
 const Img = styled.img`
     border: solid 1px black;
     border-radius: 20px;
-
-    
+    width: 962px;
+    height: 962px;
 `
 
 const AppDiv = styled.div`
